0e71ad9edddd45744f5080563548389ad0b2b724
msaenzre07 <msaenzre07@gmail.com>
2024-03-03 23:05:42 -0600

    Definiendo ajuste Form DetallesUsuarios

--- cliente/src/pages/DetallesUsuarios.jsx
+++ cliente/src/pages/DetallesUsuarios.jsx
@@ -16,38 +16,37 @@ const DetallesUsuarios= () => {
       <Helmet title="">
         <CommonSection title="Detalles de Usuarios" />
           <section>
-            <Container>
-            <Row>
-             
-          <div className="containe-detalle mt-5 shadow ">
-          <div className="row-detalle">
-          <div className="col-md-6 first_col ">
-          </div>
-
-            <div class="col-md-8 p-5 ">
-                <h2>Digite la información solicitada</h2>
-          <Form className="row g-3 detalleForm mt-4">
-            <div className="col-md-6">
-
+           
+          <div className="content-detalles ">
+          
+          <h2 className="usuarios">Digite la<span>información solicitada</span> </h2>
+          <div className="contact-wrapper animated bounceInUp">
+            <div class="contact-form">s
+            <h3>Para más información</h3>
+            <Form action="">
+            <p>
               <label htmlFor="exampleFormControlInput1" className="form-label">Nombre completo:</label>
-              <input type="text" className="form-control" id="exampleFormControlInput1"/>
-            </div>
-
+              <input type="Nombre" className="form-control" id="Nombre"/>
+            </p>
+            <p>
             <div className="col-md-6">
               <label htmlFor="exampleFormControlInput1" className="form-label">Número de cédula o de pasaporte:</label>
               <input type="text" className="form-control" id="exampleFormControlInput1"/>
             </div>
-
+            </p>
+            <p>
             <div className="col-md-6">
               <label htmlFor="exampleFormControlInput1" className="form-label">Edad:</label>
               <input type="text" className="form-control" id="exampleFormControlInput1"/>
             </div>
-
+            </p>
+            <p>
             <div className="col-md-6">
               <label htmlFor="exampleFormControlInput1" className="form-label">Correo electrónico:</label>
               <input type="text" className="form-control" id="exampleFormControlInput1"/>
             </div>
-
+            </p>
+            <p>
             <div className="titulo-imagen">
               <div className="car-titulo">
                Adjuntar foto de la "Licencia de conducir", "Cédula de Identidad" o "Pasaporte":
@@ -57,16 +56,35 @@ const DetallesUsuarios= () => {
             <label className="input-group-text" htmlfor="inputGroupFile02">Upload</label>
             </div>
             </div>
-
+            </p>
+            <p>
             <div className="mb-3">
                 <button type="submit" className="btn btn-primary mt-3"value="Submit">Enviar</button>
             </div>
+            </p>
             </Form>
             </div>
-            </div>   
+            <div class="contact-info">
+                <h4>More Info</h4>
+                <ul>
+                    <li><i class="fas fa-map-marker-alt"></i> Baker Street</li>
+                    <li><i class="fas fa-phone"></i> (111) 111 111 111</li>
+                    <li><i class="fas fa-envelope-open-text"></i> contact@website.com</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero provident ipsam necessitatibus repellendus?</p>
+                <p>Company.com</p>
             </div>
-            </Row>
-            </Container>
+
+
+
+
+
+            </div> 
+            </div>
+           
+              
+          
+         
           </section>
         </Helmet>
     );
